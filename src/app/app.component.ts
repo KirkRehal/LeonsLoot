f34a93d7578f099271e7e36a6d5416580a3d2ff1
Kirk Rehal <rehal.kirk@gmail.com>
2023-01-18 20:07:50 -0600

    add pine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public luckyDavid: boolean = true;
   title = 'LeonsLoot';
 
   constructor() {}
